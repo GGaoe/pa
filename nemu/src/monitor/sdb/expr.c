6af3641dd5ba45aa9706d9ccd29d0a0a1a603b3c
tracer-ics2023 <tracer@njuics.org>
2023-10-13 18:04:29 +0800

    >  compile NEMU 221240075 高歌 Linux ggaoe-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:29 up  5:22,  1 user,  load average: 0.98, 0.86, 0.94

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -211,6 +211,7 @@ word_t expr(char *e, bool *success) {
   }
   printf("%s\n","Position:");
   printf("%d\n",position-1);
+  printf("%d\n",tokens[0].type);
   printf("%d",eval(0,position-1));
   /* TODO: Insert codes to evaluate the expression. */
   TODO();

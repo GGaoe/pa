8ca7fd664628f7471b6efc468fa816baaf97e1ef
tracer-ics2023 <tracer@njuics.org>
2023-10-13 18:24:09 +0800

    >  compile NEMU 221240075 高歌 Linux ggaoe-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:24:09 up  5:41,  1 user,  load average: 1.24, 1.14, 0.93

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -177,7 +177,6 @@ static int The_main_op(int p,int q){
 u_int32_t eval(int p,int q) {
   int op;
   u_int32_t val1,val2;
-printf("%s","succ here");
   if (p > q) {
     printf("%s","BAD Expression");
     assert(0);
@@ -209,10 +208,12 @@ printf("%s","succ here");
 
 word_t expr(char *e, bool *success) {
   position=0;
+  printf("%s\n","succ here");
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
+  printf("%s\n","succ here");
   printf("%d",eval(0,position-1));
   /* TODO: Insert codes to evaluate the expression. */
 

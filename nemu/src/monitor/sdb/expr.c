239564175788a03425ee8aeb568359ef09564a39
tracer-ics2023 <tracer@njuics.org>
2023-10-15 16:50:13 +0800

    >  compile NEMU 221240075 高歌 Linux ggaoe-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:50:13 up 12 min,  1 user,  load average: 0.25, 0.50, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -303,11 +303,11 @@ void init_exp(){
       bool flag1 = 0;
 	    u_int32_t tmp = isa_reg_str2val(tokens[i].str, &flag1);
 	    if(flag1){
-        printf("%d\n",tmp);
+        //printf("%d\n",tmp);
 		    int_to_char(tmp, tokens[i].str);
         //printf("str:%s\ns",tokens[i].str);
        }
-      else assert(0);
+      //else assert(0);
        tokens[i].type=TK_NUM;
     }
 

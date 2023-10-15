1438e23c9ed11895e90cbd0d11d28eb1bee34259
tracer-ics2023 <tracer@njuics.org>
2023-10-15 16:41:07 +0800

    >  compile NEMU 221240075 高歌 Linux ggaoe-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:07 up 3 min,  1 user,  load average: 1.35, 1.23, 0.55

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -33,7 +33,9 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i=0;i<32;i++){
-    if(s[2]==regs[i][1]&&s[1]==regs[i][0]){*success=1;return gpr(i);}
+    if(s[2]==regs[i][1]&&s[1]==regs[i][0]){*success=1;
+    if(gpr(i)>0)return gpr(i);
+    else return -gpr(i);}
   }
   return 0;
 }

e3f31eaec2ff3961c03e722aa6f0cc29a0798dcf
tracer-ics2023 <tracer@njuics.org>
2023-11-25 13:17:39 +0800

    >  compile NEMU 221240075 高歌 Linux ggaoe-vm 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:39 up  2:16,  1 user,  load average: 0.46, 0.54, 0.40

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,7 +7,6 @@ void __am_gpu_init() {
   int i;
   AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
   int w = info.width, h = info.height;
-  //w=vgactl_port_base[0]>>32;
   uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
   for(i=0;i<w*h;i++)fb[i]=i;
   outl(SYNC_ADDR,1);
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t k=inl(KBD_ADDR);
-  kbd->keydown = k&KEYDOWN_MASK;
+  kbd->keydown = (k&KEYDOWN_MASK)?1:0;
   kbd->keycode = k&(~KEYDOWN_MASK);
 }
